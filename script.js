a9dbf983354ae2afc9ddcc9ff49e60ef8a87e188
akshaykumar <akshaykumar8734@gmail.com>
2022-12-15 18:57:00 +0530

    deleted from crud crud

--- script.js
+++ script.js
@@ -13,6 +13,8 @@
 // document.querySelector(".btn").addEventListener("mouseout", function () {
 //   this.style.backgroundColor = "green";
 // });
+var itemList = document.getElementById("users");
+itemList.addEventListener("click", removeItem);
 
 function saveData(event) {
   event.preventDefault();
@@ -41,15 +43,14 @@ function getData() {
   const users = document.getElementById("users");
 
   axios
-    .get(
-      "https://crudcrud.com/api/a65c3d8a11d341d5a89a225e951192bf/appointments"
-    )
+    .get("https://jsonplaceholder.typicode.com/users?_limit=5")
     .then((res) => {
       let data = "";
       for (let obj of res.data) {
         data += `<div class="card" style="margin: 0% 30%">
           <div class="card-body">
-            ${obj.name}
+            ${obj.name} <button id="${obj.id}" class="btn edit badge rounded-pill text-bg-success">Edit</button>
+<button id="${obj.id}" class="btn delete badge rounded-pill text-bg-danger">Delete</button>
           </div>
         </div>`;
       }
@@ -59,13 +60,41 @@ function getData() {
       console.log(err);
       users.innerHTML = `<div class="card" style="margin: 0% 30%">
   <div class="card-body">
-    ${err.response}
+    ${err.message} | ${err.code}
   </div>
 </div>`;
     });
 }
 //calling existing data if any
 getData();
+
+function removeItem(event) {
+  //delete expense
+  if (event.target.classList.contains("delete")) {
+    if (confirm("Are You Sure?")) {
+      let delete_id = event.target.id;
+      axios
+        .delete("https://jsonplaceholder.typicode.com/users/" + delete_id)
+        .then((res) => {
+          let parentDiv = event.target.parentElement.parentElement;
+          itemList.removeChild(parentDiv);
+          alert("User Deleted Successfully");
+        })
+        .catch((err) => alert(`Error: ${err.message} occurred.`));
+    }
+  }
+  //edit expense
+//   if (event.target.classList.contains("edit")) {
+//     var li = event.target.parentElement;
+//     var delete_value = event.target.parentElement.innerHTML.split("<")[0];
+
+//     itemList.removeChild(li);
+//     var values = delete_value.split("-");
+//     document.getElementById("amount").value = values[0];
+//     document.getElementById("desc").value = values[1];
+//     document.getElementById("category").value = values[2];
+//   }
+}
 
 // const btn1 = document.querySelector(".btn");
 // btn1.addEventListener("mouseout", (event) => {
