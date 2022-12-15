b05c3984ffe72aee1c22ca0e58b40d2e966882ca
akshaykumar <akshaykumar8734@gmail.com>
2022-12-15 18:28:33 +0530

    getting saved data using axios

--- script.js
+++ script.js
@@ -7,12 +7,12 @@
 //   }
 // }
 
-let btn = document.querySelector(".btn");
-btn.addEventListener("mouseover", () => (btn.style.backgroundColor = "red"));
+// let btn = document.querySelector(".btn");
+// btn.addEventListener("mouseover", () => (btn.style.backgroundColor = "red"));
 
-document.querySelector(".btn").addEventListener("mouseout", function () {
-  this.style.backgroundColor = "green";
-});
+// document.querySelector(".btn").addEventListener("mouseout", function () {
+//   this.style.backgroundColor = "green";
+// });
 
 function saveData(event) {
   event.preventDefault();
@@ -33,8 +33,39 @@ function saveData(event) {
     .then((res) => {
       console.log(res);
       alert("User Added successfully.");
-    }).catch(err => alert(err));
+    })
+    .catch((err) => alert(err));
 }
+
+function getData() {
+  const users = document.getElementById("users");
+
+  axios
+    .get(
+      "https://crudcrud.com/api/a65c3d8a11d341d5a89a225e951192bf/appointments"
+    )
+    .then((res) => {
+      let data = "";
+      for (let obj of res.data) {
+        data += `<div class="card" style="margin: 0% 30%">
+          <div class="card-body">
+            ${obj.name}
+          </div>
+        </div>`;
+      }
+      users.innerHTML = data;
+    })
+    .catch((err) => {
+      console.log(err);
+      users.innerHTML = `<div class="card" style="margin: 0% 30%">
+  <div class="card-body">
+    ${err.response}
+  </div>
+</div>`;
+    });
+}
+//calling existing data if any
+getData();
 
 // const btn1 = document.querySelector(".btn");
 // btn1.addEventListener("mouseout", (event) => {
